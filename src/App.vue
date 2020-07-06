7c9784a81f09246d51f459450645a1897013e1b3
Ваше Имя <you@example.com>
2020-07-06 07:09:06 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+      <h2 class="ml-4">Калькулятор пилометериала</h2>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <v-wood-calc />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import vWoodCalc from "./components/v-wood-calc";
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld
-  }
+    vWoodCalc
+  },
+
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/v-wood-calc.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-container>
+    <v-row class="d-block">
+      <div class="md-6">
+        <div>
+          Введите объем пиломатериала (доска, брус и т.д) в м3 (в кубометрах)
+        </div>
+        <v-text-field
+          class="result-100 md-6"
+          required
+          v-model.number="volume"
+          type="text"
+          @keyup.enter="calcWood()"
+          @change="calcWood()"
+        />
+      </div>
+      <div class="md-6">
+        <div>Введите толщину пиломатериала в мм</div>
+        <v-text-field
+          class="result-100 md-6"
+          required
+          v-model.number="thickness"
+          type="text"
+          @keyup.enter="calcWood()"
+          @change="calcWood()"
+        />
+      </div>
+      <div class="md-6">
+        <div>Введите ширину пиломатериала в мм</div>
+        <v-text-field
+          class="result-100 md-6"
+          required
+          v-model.number="height"
+          type="text"
+          @keyup.enter="calcWood()"
+          @change="calcWood()"
+        />
+      </div>
+      <div class="md-6">
+        <div>Введите длину пиломатериала в м</div>
+        <v-text-field
+          class="result-100 md-6"
+          required
+          v-model.number="length"
+          type="text"
+          @keyup.enter="calcWood()"
+          @change="calcWood()"
+        />
+      </div>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h2>Результат перевода:</h2>
+        <div v-if="flagRender">{{ outCalc }} штук</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "v-wood-calc",
+  methods: {
+    calcWood() {
+      this.flagRender = false;
+      if (
+        this.volume > 0 &&
+        this.thickness > 0 &&
+        this.height > 0 &&
+        this.length > 0
+      ) {
+        this.outCalc = Math.floor(
+          this.volume /
+            (this.thickness * this.height * this.length * 0.001 * 0.001)
+        );
+        this.flagRender = true;
+      }
+      return false;
+    }
+  },
+  data: () => ({
+    flagRender: false,
+    volume: "",
+    outCalc: "",
+    thickness: "",
+    height: "",
+    length: ""
+  })
+};
+</script>
+<style>
+.result-100 {
+  width: 200px;
+}
+</style>
