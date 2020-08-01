fe1aee958d87a4b048534e560e2fea053bbbe9ff
Ваше Имя <you@example.com>
2020-08-01 16:23:45 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="#4b1b85" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -11,24 +11,24 @@
           width="40"
         />
       </div>
-      <h2 class="ml-4">Калькулятор пилометериала</h2>
+      <h2 class="ml-4">Расчет займа</h2>
       <v-spacer></v-spacer>
     </v-app-bar>
 
     <v-main>
-      <v-wood-calc />
+      <vFinanceCalc />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import vWoodCalc from "./components/v-wood-calc";
+import vFinanceCalc from "./components/v-finance-calc";
 
 export default {
   name: "App",
 
   components: {
-    vWoodCalc
+    vFinanceCalc
   },
 
   data: () => ({
